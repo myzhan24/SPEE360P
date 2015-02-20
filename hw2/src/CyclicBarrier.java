1e2b97d824f105c8897bfaea505139fb3c10c406
myzhan24 <matthewzhan@utexas.edu>
2015-02-19 21:30:53 -0600

    jhgkjh

--- hw2/src/CyclicBarrier.java
+++ hw2/src/CyclicBarrier.java
@@ -1,15 +1,20 @@
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by Matt on 2/17/2015.
  */
 public class CyclicBarrier {
-    int total;
+    Semaphore barrier;
+    Semaphore mutex;
+
     public CyclicBarrier(int parties) {
 // Creates a new CyclicBarrier that will trip when
 // the given number of parties (threads) are waiting upon it
-        total = parties;
-
+        barrier = new Semaphore(parties);
+        mutex = new Semaphore(1);
     }
-    public synchronized int await() throws InterruptedException {
+
+    int await() throws InterruptedException {
 // Waits until all parties have invoked await on this barrier.
 // If the current thread is not the last to arrive then it is
 // disabled for thread scheduling purposes and lies dormant until
@@ -17,21 +22,18 @@ public class CyclicBarrier {
 // Returns: the arrival index of the current thread, where index
 // (parties - 1) indicates the first to arrive and zero indicates
 // the last to arrive.
+        int myTotal = 0;
 
-        int myTotal=0;
+        mutex.acquire();
+        barrier.acquire();
+        myTotal = barrier.availablePermits();
+        mutex.release();
+        if (myTotal == 0)
+            barrier.notifyAll();
+        else
+            barrier.wait();
 
-        try{
-            total--;
-            myTotal = total;
-            if(total==0)
-                notifyAll();
-
-            else
-                wait();
-
-        } finally {
-            return myTotal;
-        }
-
+        barrier.release();
+        return myTotal;
     }
 }
--- hw2/src/FairReadWrite.java
+++ hw2/src/FairReadWrite.java
@@ -8,20 +8,42 @@ public class FairReadWrite {
     ReentrantLock lock = new ReentrantLock();
     Condition writers = lock.newCondition();
     Condition readers = lock.newCondition();
+    ReentrantLock mutexReadCount = new ReentrantLock();
     int numWriters = 0;
     int numReaders = 0;
 
-    void beginRead() throws InterruptedException {
+    void beginRead(){
         lock.lock();
-        try{
-            while(numWriters!=0)
-                readers.await();
+        try {
+            while (numWriters != 0)
+                try {
+                    readers.await();        //reader wait
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-            numReaders++;
+            try {
+                mutexReadCount.lock();              //reader count mutex
+                numReaders++;
+                if (numReaders == 1)             //reader waits for writer
+                    try {
+                        writers.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+            } finally {
+                mutexReadCount.unlock();            //reader count mutex unlock
+            }
+
+            readers.signal();               //signal next reader
+        } catch (Exception e) {
+
         } finally {
             lock.unlock();
         }
     }
+
     void endRead() {
         lock.lock();
         try{
@@ -29,21 +51,29 @@ public class FairReadWrite {
             if(numReaders==0)
                 writers.signal();
 
-
         } finally {
             lock.unlock();
         }
     }
-    void beginWrite() throws InterruptedException {
-        lock.lock();
-        try{
-            while(numWriters!=0 || numReaders!=0)
-                writers.await();
+    void beginWrite(){
+        while(numWriters!=0 || numReaders!=0) {
 
-            numWriters++;
-        } finally {
-            lock.unlock();
+            if(numReaders!=0)
+                try {
+                    readers.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            else if(numWriters!=0)
+                try {
+                    writers.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
         }
+        numWriters++;
+
     }
     void endWrite(){
         lock.lock();
